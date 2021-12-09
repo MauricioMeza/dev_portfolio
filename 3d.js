1db14c37c588c8cbe8f1323c743ae6a45d3cd95f
Mauricio Meza <mmezab@unal.edu.co>
2021-12-09 01:15:50 -0500

    load test final

--- 3d.js
+++ 3d.js
@@ -224,8 +224,10 @@ function loadModels(){
 //---FINISH LOADING---
 function loaded(){
    return new Promise(resolve => {
-      load.style.opacity = 0;
-      load.style.visibility = "hidden";
+      setTimeout(() => {
+         load.style.opacity = 0;
+         load.style.visibility = "hidden";       
+      }, 250);
       resolve('ok')     
    });
 }
